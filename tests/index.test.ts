5302b459ae2d0f995677f7f4470aad1f231ab2c6
zouhang <1102639563@qq.com>
2022-08-09 11:48:48 +0800

    chore: add test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,2 +1,20 @@
-// todo
-export {}
+import { createGenerator, escapeSelector } from 'unocss'
+import presetUno from '@unocss/preset-uno'
+import { describe, expect, test } from 'vitest'
+import presetPrefixer from '../src'
+
+describe('preset-prefixer', () => {
+  const uno = createGenerator({
+    presets: [
+      presetUno(),
+      presetPrefixer()
+    ],
+  })
+
+  test('flex autoprefixer', async () => {
+
+    const { css } = await uno.generate(['flex'], { preflights: false})
+
+    expect(css).toBe(`/* layer: default */\n.flex{display:-webkit-box;display:-webkit-flex;display:flex;}`)
+  })
+})
