3033a6f72da6c4ef0017b308e40327233d37c9f1
Chris <hizyyv@gmail.com>
2024-08-21 16:46:12 +0800

    chore: upgrade

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,44 +1,51 @@
-import { createGenerator, escapeSelector } from 'unocss'
+import { createGenerator } from 'unocss'
 import presetUno from '@unocss/preset-uno'
 import { describe, expect, test } from 'vitest'
 import presetPrefixer from '../src'
 
 describe('preset-prefixer', () => {
   const uno = createGenerator({
-    presets: [presetUno(), presetPrefixer()],
+    presets: [
+      presetUno(),
+      presetPrefixer(),
+    ],
   })
 
+  function generate(input: Parameters<typeof uno.generate>[0]) {
+    return uno.generate(input, { preflights: false })
+  }
+
   test('flex autoprefixer', async () => {
-    const { css } = await uno.generate(['flex'], { preflights: false })
+    const { css } = await generate(['flex'])
 
     expect(css).toMatchSnapshot()
   })
 
   test('text space', async () => {
-    const { css } = await uno.generate(['text-1'], { preflights: false })
+    const { css } = await generate(['text-1'])
 
     expect(css).toMatchSnapshot()
   })
 
   test('font weight', async () => {
-    const { css } = await uno.generate(['font-400', 'font-bold'], { preflights: false })
+    const { css } = await generate(['font-400', 'font-bold'])
 
     expect(css).toMatchSnapshot()
   })
 
   test('background gradient', async () => {
-    const { css } = await uno.generate(['bg-gradient-to-tr', 'from-purple-500', 'to-pink-500'], { preflights: false })
+    const { css } = await generate(['bg-gradient-to-tr', 'from-purple-500', 'to-pink-500'])
 
     expect(css).toMatchSnapshot()
   })
 
   test('p-x', async () => {
-    const { css } = await uno.generate(['px-10'], { preflights: false })
+    const { css } = await generate(['px-10'])
 
     expect(css).toMatchSnapshot()
   })
   test('space-x', async () => {
-    const { css } = await uno.generate(['space-x-10'], { preflights: false })
+    const { css } = await generate(['space-x-10'])
 
     expect(css).toMatchSnapshot()
   })
