ad6c78306e132da50aa096006459c2c6aba8693f
Chris <hizyyv@gmail.com>
2024-09-23 11:44:21 +0800

    pref: reuse the same targets

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import process from 'node:process'
 import browserslist from 'browserslist'
 import { browserslistToTargets, transformStyleAttribute } from 'lightningcss'
 
-export default function autoprefixerPreset(targets: string[] = [
+export default function autoprefixerPreset(queries: string[] = [
   '>= 0.25%',
   'last 2 versions',
   'not dead',
@@ -12,6 +12,8 @@ export default function autoprefixerPreset(targets: string[] = [
   'Android >= 4.0',
   'iOS >= 8',
 ]): Preset {
+  const targets = browserslistToTargets(browserslist(queries))
+
   return {
     name: 'unocss-preset-autoprefixer',
     postprocess: (util) => {
@@ -21,7 +23,7 @@ export default function autoprefixerPreset(targets: string[] = [
       const entries = util.entries
       const { code } = transformStyleAttribute({
         code: Buffer.from(entries.filter(item => !item[0].startsWith('--un')).map(x => x.join(':')).join(';')),
-        targets: browserslistToTargets(browserslist(targets)),
+        targets,
         minify: true,
       })
 
